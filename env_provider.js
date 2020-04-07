5cae7986cf90ad33e5ff5698f7ec8efd29f3e9bf
Mohamedaly995 <m.aly@cognitev.com>
2020-04-07 23:07:34 +0200

    Adding sequelize config and fixing envs

--- env_provider.js
+++ env_provider.js
@@ -4,8 +4,8 @@ const SRV_PORT = process.env.PORT || '8090';
 const SRV_HOST = process.env.HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || '3306';
 const DB_DATABASE = process.env.DB_DATABASE || 'swmac';
-const DB_USERNAME = process.env.DB_DATABASE || 'swmac';
-const DB_PASSWORD = process.env.DB_DATABASE || 'password';
+const DB_USERNAME = process.env.DB_USERNAME || 'root';
+const DB_PASSWORD = process.env.DB_PASSWORD || 'password';
 const DB_HOST = process.env.DB_HOST || 'localhost';
 
 module.exports = {
