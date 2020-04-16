a731d0fe91ac4670f42c8e497ddad6bb4a6cc2b4
3bhady <mohamed_abdelhady96@hotmail.com>
2020-04-17 01:59:21 +0200

    organizer

--- client/client.js
+++ client/client.js
@@ -1,5 +1,12 @@
 // import './socket.io.js' as io
 // io = require('./socket.io.js');
 
-var socket = io('http://localhost:3000');
+// var socket = io('http://localhost:3000');
+var socket = io('http://localhost:3000',{query:'roomName=roomy&action=create' });
 // socket.emit('room', 'room1');
+
+function send(){
+	console.log("test");
+	val = document.getElementById('action').value;
+	socket.emit('update',{action:val});
+}
