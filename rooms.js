75b3621dd34bf9e5485ee3d3b027d04b46a2241a
Mohamedaly995 <m.aly@cognitev.com>
2020-04-16 22:08:46 +0200

    Adding organizer and users

--- rooms.js
+++ rooms.js
@@ -1,49 +1,124 @@
 const envProvider = require('./env_provider');
 const log = require('simple-node-logger').createSimpleLogger();
 
+
+
 class Room{
-  constructor(name, socket /*io.socket*/){
-    this.name = name;
+  constructor(organizer, roomID, url='url'){
+    this.id = roomID;
     this.url = url;
-    this.socket = socket;
-    this.users = [];
+    this.organizer = organizer;
   };
-  addUser(user){
-    this.users.push(user);
-    return this.users;
-  };
+
 }
 
 class User{
-  constructor(user_id, room_id){
-    this.user_id = user_id;
-    this.room = room_id;
+  constructor(organizer, userSocket){
+    log.info(`New user created from the organizer, userID:${userSocket.id}`)
+    this.organizer = organizer
+    this.socket = userSocket;
+    this.id = userSocket.id;
+    this.joinedRooms = {}; // Represents actuall joined rooms, json for fast lookup
+    
+    this.eventHandlers = {
+      // joinRoom: this.joinRoom.bind(this) // Handles new socket joined a room; {eventName: callbackFunction}
+      joinRoom: this.joinRoom.bind(this),
+      createRoom: this.createRoom.bind(this)
+    }
+
+    this.attachUserEvents();
   }
+
+  // Attaching events for a user, ex{join a room, send some data, disconnect..}
+  // Each user is responsible for handling its own events, sending updates for organizer (master)
+  // Organizer can change some users update frequence (throttle some users)
+  // It can also neglect user updates
+
+  attachUserEvents() {
+    for (var event in this.eventHandlers){
+      this.socket.on(event, this.eventHandlers[event]);
+    }
+    // log.info("user: " , socket.id , " has connected!!");
+    // socket.on('room', this.joinRoom);
+    // socket.on('disconnecting', this.leaveRoom);
+  }
+
+
+  // User joined a room (or create if not exist)
+  joinRoom(data){
+    var roomID = data.roomID;
+    // User can get permission to join only created a room
+    var roomExists = this.organizer.requestJoinRoom(this.socket, roomID);
+    if(roomExists){
+      this.socket.join(roomID);
+      this.organizer.notify('roomJoined', this, '');
+    }else{
+      // Organizer(master) should take appropriate action with client
+      // as the user is trying to join a room that is not created yet.
+      this.organizer.notify('failedToJoinRoom', this, 'Room not created yet')
+    }
+  }
+
+  // createRoom(data){
+
+  // }
 }
+
+
 
 class Organizer{
 
-  constructor(port){
-    this.rooms = [];
-    this.users = [];
+  constructor(socketPort){
+    this.rooms = {}; //json object instead of array for fast lookup {id:roomObject}
+    this.users = {}; // Json object instead of array for fast lookup {id: userObject}
     
-    this.sockets = require('socket.io')(port, {
+    this.allSockets = require('socket.io')(socketPort, {
       serveClient: false,
       cookie: false
     }).sockets;
+    
+    this.eventHandlers = {
+      connection: this.newConnection.bind(this)
+      // joinRoom: this.joinRoom.bind(this) // Handles new socket joined a room; {eventName: callbackFunction}
+    }
 
-    this.attachEvents();
+    this.attachOrganizerEvents();
   }
 
-  attachEvents(){
-    this.sockets.on('connection', this.connect.bind(this));
+  // New connection
+  newConnection(socket){
+    // Add new user to users list
+    let userID = socket.id
+    this.users.userID = new User(this, socket);
+    //TODO: log and update stats
   }
 
-  connect(socket) {
-    log.info("user: " , socket.id , " has connected!!");
-    socket.on('room', this.joinRoom);
-    socket.on('disconnecting', this.leaveRoom);
+  // attach events to new connection
+  attachOrganizerEvents() {
+    for (var event in this.eventHandlers){
+      this.allSockets.on(event, this.eventHandlers[event]);
+    }
+    // log.info("user: " , socket.id , " has connected!!");
+    // socket.on('room', this.joinRoom);
+    // socket.on('disconnecting', this.leaveRoom);
   }
+
+  requestJoinRoom(socket, roomID){
+    // Check if room exist: if exist, and user is valid to join then return room, else return None
+    // If room created before.
+    if(this.rooms.hasOwnProperty(roomID)){
+      return true;
+    } else{ // Room not created
+      return false;
+    }
+  }
+
+  // Organizer should process notifications in order to keep in-sync with updates
+  notify(action, caller, message){
+    var callerType = ((caller instanceof User) ? 'User' : 'Room'); 
+    log.info(`action: ${action} is submitted to the organizer from slave-type:${callerType} and id:${caller.id}`)
+  }
+
   joinRoom(room){
     let socket = this;
     if(Object.keys(socket.rooms).length!=1){
@@ -57,7 +132,6 @@ class Organizer{
     log.info("joined room:", room);
   }
 
-
   leaveRoom(err){
     socket = this;
     log.info(socket.room);
@@ -66,5 +140,4 @@ class Organizer{
   }
 
 }
-
 const organizer = new Organizer(envProvider.IO_PORT);
