480bd2c2fa09fd6b0226b67af1a19335af65b558
Mohamedaly995 <m.aly@cognitev.com>
2020-04-24 21:10:38 +0200

    Moving init socketServer to app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const log = require('simple-node-logger').createSimpleLogger();
+const log = require("simple-node-logger").createSimpleLogger();
 
-const http = require('http');
+const http = require("http");
 
-const envProvider = require('./env_provider');
+const envProvider = require("./env_provider");
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 const port = envProvider.SRV_PORT;
 
-app.set('port', port);
+app.set("port", port);
 
 const server = http.createServer(app);
 
@@ -21,28 +21,28 @@ organizer = new Organizer(server);
 
 server.listen(port);
 
-server.on('error', onError);
-server.on('listening', onListening);
+server.on("error", onError);
+server.on("listening", onListening);
 
 /**
  * Event listener for HTTP server "error" event.
  * @param {Object} error returned.
  */
 function onError(error) {
-  if (error.syscall !== 'listen') {
+  if (error.syscall !== "listen") {
     throw error;
   }
 
-  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
-      log.error(bind + ' requires elevated privileges');
+    case "EACCES":
+      log.error(bind + " requires elevated privileges");
       process.exit(1);
       break;
-    case 'EADDRINUSE':
-      log.error(bind + ' is already in use');
+    case "EADDRINUSE":
+      log.error(bind + " is already in use");
       process.exit(1);
       break;
     default:
@@ -57,7 +57,7 @@ function onListening() {
   const addr = server.address();
   const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
   log.info(bind);
-  log.info('server started successfully, listening to : ' + addr.port);
+  log.info("server started successfully, listening to : " + addr.port);
 }
 
 module.exports = app;
